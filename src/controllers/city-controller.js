18ad4b83fa85e64a108a3e184a00c2ccc8772898
kunal pal <kunalpal2292004@gmail.com>
2023-07-09 13:39:20 +0530

    Added the city controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,99 @@
+const {CityService} = require('../services/index');
+
+
+const cityService = new CityService();
+
+const create = async (req, res) =>{
+    try{
+        const city = await cityService.createCity(req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message: 'Successfully created a city',
+            err:{}
+        })
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message: 'Not able to cerate a city',
+            err:error
+        });
+    }
+    
+}
+const destroy = async(req, res) =>{
+    try{
+        const city = await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message: 'Successfully deleted a city',
+            err:{}
+        });
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message: 'Not able to delete the city',
+            err:error
+        });
+    }
+    }
+// GRT -> /city/:id
+
+const get = async(req, res) =>{
+    try{
+        const city = await cityService.getCity(req.params.id);
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message: 'Successfully fetched a city',
+            err:{}
+        });
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message: 'Not able to get the city',
+            err:error
+        });
+    }
+
+}
+
+//patch -> /city/id:
+const update = async(req, res) =>{
+    try{
+        const city = await cityService.updateCity(req.params.id.req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message: 'Successfully updated the  city',
+            err:{}
+        })
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            message: 'Not able to update the city',
+            err:error
+        });
+    }
+
+}
+
+module.exports = {
+    create,
+    destroy,
+    get,
+    update
+}
--- src/services/city-service.js
+++ src/services/city-service.js
@@ -2,12 +2,12 @@ const {CityRepository} = require('../repository/index');
 
 class CityService {
     constructor(){
-        this.CityRepository = new CityRepository();
+        this.cityRepository = new CityRepository();
 
     }
     async createCity(data) {
         try{
-            const city = await this.CityRepository.createCity(data);
+            const city = await this.cityRepository.createCity(data);
             return city;
 
         }
@@ -19,7 +19,7 @@ class CityService {
     }
     async deleteCity(cityId){
         try{
-            const response = await this.CityRepository.deleteCity(cityId);
+            const response = await this.cityRepository.deleteCity(cityId);
             return response;
     }
         catch(error){
@@ -29,7 +29,7 @@ class CityService {
     }
     async updateCity(cityId,data){
         try{
-            const city = await this.CityRepository.updateCity(cityId,data);
+            const city = await this.cityRepository.updateCity(cityId,data);
             return city;
         }
         catch(error){
@@ -39,7 +39,7 @@ class CityService {
     }
     async getCity(cityId) {
         try{
-            const city = await this.CityRepository.getCity(cityId);
+            const city = await this.cityRepository.getCity(cityId);
             return city;
         }
         catch(error){
